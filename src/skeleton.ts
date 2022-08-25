6c95e4a0967e820fd10a270b7facda3ecb1b8f36
purpurlu <laurazurbayeva@gmail.com>
2022-08-25 09:37:22 +0100

    add solution

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -1,11 +1,59 @@
+interface MarkedGuess {
+  lettersWithScores: LetterWithScore[]
+}
+
+interface LetterWithScore {
+  letter: string,
+  score: OneLetterScore
+}
+
+type OneLetterScore = "fully correct" | "partially correct" | "incorrect"
+
 /**
  * Adds together two numbers
  * @param a - the first number to add
  * @param b - the second number to add
  * @returns the total
  */
-function sum(a: number, b: number): number {
-  return a + b;
+
+/* PSEUDOCODE
+  create returnGuess = {}
+  for i in guess
+    if guess[i] === hiddenGuess[i]  then 
+      add to returnGuess guess[i] with 'fully correct'
+      else if hiddenTarget.includes(guess[i]) {
+        add to returnGuess guess[i] with 'partially correct'
+      }
+      else add to returnGuess guess[i] with ' incorrect'
+    }
+    
+
+
+*/
+function markWordleGuess(guess: string, hiddenTarget: string): MarkedGuess {
+
+  const guessArr = guess.split("")
+  const hiddenTargetArr = hiddenTarget.split("")
+  const returnGuess: MarkedGuess = {lettersWithScores: []}
+    for (let i in guessArr) {
+      if (guessArr[i] === hiddenTargetArr[i]) {
+              returnGuess.lettersWithScores.push({letter: guessArr[i], score: "fully correct"})
+      }
+      else if (hiddenTarget.includes(guessArr[i])) {
+        returnGuess.lettersWithScores.push({letter: guessArr[i], score: "partially correct"})
+      }
+      else {
+        returnGuess.lettersWithScores.push({letter: guessArr[i], score: "incorrect"})
+      }
+    }
+
+
+  return returnGuess;
 }
 
-export default sum;
+export default markWordleGuess;
+
+const guess = "apples"
+const hiddenTarget = "party"
+const result = markWordleGuess(guess, hiddenTarget)
+console.log(result)
